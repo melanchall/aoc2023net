b5c42c16f06afd7aaf631e710e3d4e91622af4f9
Maxim Dobroselsky <m.hall.muz@gmail.com>
2023-12-05 19:30:23 +0300

    Day 5 solved

--- Aoc2023Net/Days/Day5.cs
+++ Aoc2023Net/Days/Day5.cs
@@ -1,11 +1,125 @@
-﻿// xxx: https://adventofcode.com/2023/day/5
+﻿// If You Give A Seed A Fertilizer: https://adventofcode.com/2023/day/5
+
+using System.Text.RegularExpressions;
 
 namespace Aoc2023Net.Days
 {
     internal sealed class Day5 : Day
     {
-        public override object SolvePart1() => null;
+        private record Range(long SourceStart, long DestinationStart, long Length);
 
-        public override object SolvePart2() => null;
+        private sealed class Map
+        {
+            private readonly Range[] _ranges;
+
+            private readonly long _minSource;
+            private readonly long _maxSource;
+            private readonly long _minDestination;
+            private readonly long _maxDestination;
+
+            public Map(Range[] ranges)
+            {
+                _ranges = ranges;
+
+                _minSource = _ranges.Min(r => r.SourceStart);
+                _maxSource = _ranges.Max(r => r.SourceStart + r.Length - 1);
+
+                _minDestination = _ranges.Min(r => r.DestinationStart);
+                _maxDestination = _ranges.Max(r => r.DestinationStart + r.Length - 1);
+            }
+
+            public long GetDestination(long source)
+            {
+                if (source < _minSource || source > _maxSource)
+                    return source;
+
+                foreach (var r in _ranges)
+                {
+                    if (source < r.SourceStart || source >= r.SourceStart + r.Length)
+                        continue;
+
+                    return r.DestinationStart + (source - r.SourceStart);
+                }
+
+                return source;
+            }
+
+            public long GetSource(long destination)
+            {
+                if (destination < _minDestination || destination > _maxDestination)
+                    return destination;
+
+                foreach (var r in _ranges)
+                {
+                    if (destination < r.DestinationStart || destination >= r.DestinationStart + r.Length)
+                        continue;
+
+                    return r.SourceStart + (destination - r.DestinationStart);
+                }
+
+                return destination;
+            }
+        }
+
+        public override object SolvePart1()
+        {
+            var (seeds, maps) = GetSeedsAndMaps();
+            return seeds.Min(s => GetLocation(s, maps));
+        }
+
+        public override object SolvePart2()
+        {
+            var (seeds, maps) = GetSeedsAndMaps();
+            var seedsRanges = seeds.Chunk(2).Select(c => (Start: c[0], Length: c[1])).ToArray();
+
+            var heuristicSeedRangeSteps = 1000;
+            var minLocation = seedsRanges
+                .SelectMany(r => Enumerable
+                    .Range(0, heuristicSeedRangeSteps)
+                    .Select(i => r.Start + (r.Length / heuristicSeedRangeSteps) * i))
+                .Select(s => GetLocation(s, maps))
+                .Min();
+
+            maps = maps.Reverse().ToArray();
+
+            for (var location = minLocation - 1; location >= 0; location--)
+            {
+                var seed = maps.Aggregate(location, (r, m) => m.GetSource(r));
+                if (seedsRanges.Any(r => r.Start <= seed && seed < r.Start + r.Length))
+                    minLocation = location;
+            }
+
+            return minLocation;
+        }
+
+        private long GetLocation(long seed, Map[] maps) =>
+            maps.Aggregate(seed, (result, m) => m.GetDestination(result));
+
+        private (long[] Seeds, Map[] Maps) GetSeedsAndMaps()
+        {
+            var groups = InputData.GetInputLinesGroups();
+
+            var seeds = Regex
+                .Matches(groups[0].First(), @"\d+")
+                .Select(m => long.Parse(m.Value))
+                .ToArray();
+
+            return (seeds, new[]
+            {
+                GetMap(groups[1]),
+                GetMap(groups[2]),
+                GetMap(groups[3]),
+                GetMap(groups[4]),
+                GetMap(groups[5]),
+                GetMap(groups[6]),
+                GetMap(groups[7])
+            });
+        }
+
+        private Map GetMap(string[] rangesStrings) => new Map(rangesStrings
+            .Skip(1)
+            .Select(s => s.Split(' ').Select(long.Parse).ToArray())
+            .Select(p => new Range(p[1], p[0], p[2]))
+            .ToArray());
     }
 }
--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 |2|[Cube Conundrum](https://github.com/melanchall/aoc2023net/blob/main/Aoc2023Net/Days/Day2.cs)|:star::star:|
 |3|[Gear Ratios](https://github.com/melanchall/aoc2023net/blob/main/Aoc2023Net/Days/Day3.cs)|:star::star:|
 |4|[Scratchcards](https://github.com/melanchall/aoc2023net/blob/main/Aoc2023Net/Days/Day4.cs)|:star::star:|
+|5|[If You Give A Seed A Fertilizer](https://github.com/melanchall/aoc2023net/blob/main/Aoc2023Net/Days/Day5.cs)|:star::star:|
